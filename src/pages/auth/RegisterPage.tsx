5da53d8d28469ae348a19915b5e6888a42e2a0e7
Erick Jaquin <erick_libarino2016@outlook.com>
2026-05-27 18:52:44 -0300

    nada

---
+++ src/pages/auth/RegisterPage.tsx
@@ -0,0 +1,21 @@
+export function RegisterPage() {
+    return (
+        <main>
+            <section>
+                <h1>Cadastre-se</h1>
+                <form>
+                    <div>
+                        <label>Nome</label>
+                        <input/>
+                        <label>email</label>
+                        <input/>
+                        <label>CPF</label>
+                        <input/>
+                        <label>Data de Nascimento</label>
+                        <input/>
+                    </div>
+                </form>
+            </section>
+        </main>
+    )
+}
