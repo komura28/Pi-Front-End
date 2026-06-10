0a1c4aa9e061a0de59e5d342bc6eb13f11a0c287
komura <erickyuukomura@gmail.com>
2026-06-10 01:41:21 -0300

    Desabilitando botão de candidatar-se apos candidatar

--- src/components/CardTurma.tsx
+++ src/components/CardTurma.tsx
@@ -1,7 +1,9 @@
 
-import { solicitarMatricula } from "../services/matriculaService";
-import type { authTurma, TurmaMatricula } from "../types/auth/auth-types";
+import { useEffect, useState } from "react";
+import { meusCursos, solicitarMatricula } from "../services/matriculaService";
+import type { TurmaMatricula } from "../types/auth/auth-types";
 import { formatDate } from "../utils/formatters";
+import type { authMatricula } from "../types/matricula/matricula-types";
 
 
 type CardProps = {
@@ -12,15 +14,45 @@ export function CardTurma({ turma }: CardProps) {
 
     const dataInicio = formatDate(turma.turma.dataInicio);
     const dataFim = formatDate(turma.turma.dataFim);
+    const [candidaturas, setCandidaturas] = useState<authMatricula[]>([]);
+    const [error, setError] = useState("");
+    const [candidatura, setCandidatura] = useState(false);
+    const jaCandidatado = candidaturas.some(
+        solicitacao => solicitacao.turma._id === turma.turma._id
+    );
+
     const handleCandidatar = async () => {
         try {
-            await solicitarMatricula({ turma: turma.turma._id });
+            const novaCandidatura = await solicitarMatricula({
+                turma: turma.turma._id
+            });
+
+            setCandidaturas(prev => [...prev, novaCandidatura]);
             alert("Candidatura enviada com sucesso!");
         } catch (error) {
             console.error("Erro ao candidatar-se à turma:", error);
             alert("Erro ao enviar candidatura. Por favor, tente novamente.");
         }
     }
+
+    useEffect(() => {
+        async function buscarCandidaturas() {
+            try {
+                setError("");
+                const data = await meusCursos();
+                setCandidaturas(data);
+            } catch (error) {
+                setError("Erro ao carregar os dados do Servidor");
+            }
+        }
+
+        buscarCandidaturas();
+        candidaturas.forEach(solicitacao => {
+            if (solicitacao.turma._id == turma.turma._id) {
+                setCandidatura(true);
+            }
+        });
+    }, []);
 
     return (
         <div className="max-w-md">
@@ -48,9 +80,14 @@ export function CardTurma({ turma }: CardProps) {
 
                 <button
                     onClick={handleCandidatar}
-                    className="w-full rounded-lg bg-blue-600 px-4 py-2 font-semibold text-white transition hover:bg-blue-700"
+                    disabled={jaCandidatado}
+                    className={`w-full rounded-lg px-4 py-2 font-semibold text-white transition
+        ${jaCandidatado
+                            ? "bg-gray-400 cursor-not-allowed"
+                            : "bg-blue-600 hover:bg-blue-700"
+                        }`}
                 >
-                    Candidatar-se
+                    {jaCandidatado ? "Já Candidatado" : "Candidatar-se"}
                 </button>
             </div>
         </div>
