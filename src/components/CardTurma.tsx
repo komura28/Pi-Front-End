f4075f984c80c8cbc703fff3adfcc615af1a6933
Vitor <vitor.nogue316@gmail.com>
2026-06-09 22:53:41 -0300

    Arrumando a lógica de vagas disponiveis

--- src/components/CardTurma.tsx
+++ src/components/CardTurma.tsx
@@ -1,20 +1,20 @@
 
 import { solicitarMatricula } from "../services/matriculaService";
-import type { authTurma } from "../types/auth/auth-types";
+import type { authTurma, TurmaMatricula } from "../types/auth/auth-types";
 import { formatDate } from "../utils/formatters";
 
 
 type CardProps = {
-    turma: authTurma;
+    turma: TurmaMatricula;
 }
 
 export function CardTurma({ turma }: CardProps) {
 
-    const dataInicio = formatDate(turma.dataInicio);
-    const dataFim = formatDate(turma.dataFim);
+    const dataInicio = formatDate(turma.turma.dataInicio);
+    const dataFim = formatDate(turma.turma.dataFim);
     const handleCandidatar = async () => {
         try {
-            await solicitarMatricula({ turma: turma._id });
+            await solicitarMatricula({ turma: turma.turma._id });
             alert("Candidatura enviada com sucesso!");
         } catch (error) {
             console.error("Erro ao candidatar-se à turma:", error);
@@ -26,13 +26,13 @@ export function CardTurma({ turma }: CardProps) {
         <div className="max-w-md">
             <div className="rounded-xl bg-white p-6 shadow-md transition">
                 <h2 className="mb-2 text-2xl font-semibold text-gray-800">
-                    Turma {formatDate(turma.dataInicio)}
+                    Turma {formatDate(turma.turma.dataInicio)}
                 </h2>
 
                 <div className="mb-4 space-y-2 text-gray-600">
                     <p>
                         <span className="font-medium">Curso:</span>{" "}
-                        {turma.curso?.name}
+                        {turma.turma.curso?.name}
                     </p>
 
                     <p>
@@ -42,7 +42,7 @@ export function CardTurma({ turma }: CardProps) {
 
                     <p>
                         <span className="font-medium">Vagas:</span>{" "}
-                        {turma.capacidade} vagas disponíveis
+                        {turma.vagasDisponiveis} vagas disponíveis
                     </p>
                 </div>
 
